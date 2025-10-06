b54af4a0fb5b6200618a54742aff04131ed627c4
mjavurek <57668028+mjavurek@users.noreply.github.com>
2025-10-06 11:26:40 +0200

    "amount" is now dict for newer transactions

--- python/import-elba/transactions.py
+++ python/import-elba/transactions.py
@@ -1,25 +1,26 @@
+# Diese Datei im Home-Verzeichnis ablegen
+elba_transactions_file = "Downloads/bank_transactions.json"
+database_export_file = "Downloads/bank_transactions.csv"
+database_import_file = "Downloads/bank_transactions_import.csv"
+
 import csv
 import json
-
-NULL = ""
 
 def quote(s):
     if s=="NULL": return s
     return '"'+s+'"'
 
 
-filename = "bank_transactions.json"
-print("--- lade", filename, "---------------------------")
-with open(filename) as f:
+print("--- lade", elba_transactions_file, "---------------------------")
+with open(elba_transactions_file) as f:
     transactions = f.read()
 transactions = json.loads(transactions)
 for t in transactions[:5]:
    print(t,"\n")
 print("   ...\n")
 
-filename = "Downloads/bank_transactions.csv"
-print("--- lade", filename, "---------------------------")
-with open(filename) as f:
+print("--- lade", database_export_file, "---------------------------")
+with open(database_export_file) as f:
     fs_bank_transactions = [fst for fst in csv.reader(f, delimiter=",", quotechar='"')] 
 keys = fs_bank_transactions[0]
 index = {key:i for i,key in enumerate(keys)}
@@ -47,17 +48,22 @@ for t in reversed(transactions):
             break
     text = t.get("transaktionsteilnehmerZeile1", "NULL")
     iban = t.get("auftraggeberIban", "NULL")
+    betrag = t["betrag"]["amount"]
+    if isinstance(betrag, dict):
+        betrag = betrag["source"]
+    betrag = float(betrag)
+    transaction_str = " ".join(["%11s" % t["id"], t["buchungstag"], "%8.2f €" % betrag, zr, text])
     if t["id"] in fs_ids: 
         print("#", end="")
-        print("%11s" % t["id"], t["buchungstag"],"%8.2f €" % t["betrag"]["amount"], zr, text)
+        print(transaction_str)
     else: 
-        print("%11s" % t["id"], t["buchungstag"],"%8.2f €" % t["betrag"]["amount"], zr, text) 
+        print(transaction_str) 
         transactions_to_import.append({
             "id": str(fs_id),
             "bank_account_id": ba_id,
             "external_id": t["id"],
             "date": t["buchungstag"],
-            "amount": str(t["betrag"]["amount"]),
+            "amount": "%.2f" % betrag,
             "iban": iban,
             "reference": quote(zr),
             "text": quote(text),
@@ -69,12 +75,11 @@ print("------------------------------")
 if len(transactions_to_import)==0:
     print("keine neuen Transaktionen.")
 else:
-    filename = "Downloads/bank_transactions_import.csv"
-    with open(filename, "w") as f:
+    with open(database_import_file, "w") as f:
         for t in transactions_to_import:
             line = ",".join(t.values())
             print(line)
             # print("  ",t,"\n")
             f.write(line+"\n")
-    print("zu importierende Bankdaten geschrieben in", filename)    
+    print("zu importierende Bankdaten geschrieben in", database_import_file)    
   
